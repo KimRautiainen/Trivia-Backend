352ad3666766e8dfc086533dd4095ff6b10255e9
Kim Rautiainen <kim.rautiainen@hotmail.com>
2024-01-23 17:10:01 +0200

    Update leaderboard on highscore done

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -19,9 +19,32 @@ const getLeaderboardById = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+const getHighscore = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const highscore = await leaderboardModel.getHighscore(userId);
+        res.json(highscore);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+const updateHighscore = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const gameId = req.body.gameId;
+        const score = req.body.score;
+        const highscore = await leaderboardModel.updateHighscore(userId, gameId, score);
+        res.json(highscore);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 
 const leaderboardController = {
   getLeaderboard,
   getLeaderboardById,
+  getHighscore,
+  updateHighscore,
 };
 module.exports = leaderboardController;
--- routes/leaderboardRoute.js
+++ routes/leaderboardRoute.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const authorizeUser = require("../middleware/authMiddleware");
 const leaderboardController = require("../controllers/leaderboardController");
+const passport = require("passport");
 
 // Leaderboard routes
 router.route("/getLeaderboard").get(leaderboardController.getLeaderboard);
@@ -12,4 +13,19 @@ router.get(
   "/getLeaderboardById/:userId",
   leaderboardController.getLeaderboardById
 );
+
+// get current highscore of user
+router.get(
+  "/getHighscore/:userId",
+  leaderboardController.getHighscore
+);
+
+// update highscore
+router.put(
+    "/updateHighscore/:userId",
+    passport.authenticate('jwt', { session: false }),
+    authorizeUser,
+    leaderboardController.updateHighscore
+  );
+
 module.exports = router;
